c0e3847bcaa1edb736dadd9e62227be421cd548d
Michael C. <68439689+MC-intel@users.noreply.github.com>
2025-03-05 22:04:13 -0600

    Fix error logging in output plugins

--- output.go
+++ output.go
@@ -15,28 +15,3 @@ type Output interface {
 	// Write takes in group of points to be written to the Output
 	Write(metrics []Metric) error
 }
-
-// AggregatingOutput adds aggregating functionality to an Output.  May be used
-// if the Output only accepts a fixed set of aggregations over a time period.
-// These functions may be called concurrently to the Write function.
-type AggregatingOutput interface {
-	Output
-
-	// Add the metric to the aggregator
-	Add(in Metric)
-	// Push returns the aggregated metrics and is called every flush interval.
-	Push() []Metric
-	// Reset signals that the aggregator period is completed.
-	Reset()
-}
-
-// Enhanced Write function to track errors
-func (o *Output) Write(metrics []Metric) error {
-	err := o.Write(metrics)
-	if err != nil {
-		log.Println("Error writing metrics:", err)
-		// Increment error counter
-		metrics[0].AddField("error_count", 1)
-	}
-	return err
-}
